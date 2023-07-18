7d92b309f254c169b2506932cbb7c3636ea74d3b
Ricocotam <ricocotam@gmail.com>
2023-07-18 10:40:01 +0200

    dl comediehumaine

---
+++ tp1/comediehumaine.py
@@ -0,0 +1,19 @@
+from tqdm import tqdm
+import requests
+from bs4 import BeautifulSoup
+
+comediehumaine_url = "https://www.gutenberg.org/ebooks/search/?query=la+com%C3%A9die+humaine&submit_search=Go%21"
+link_format = "https://www.gutenberg.org/{text_id}.txt.utf-8"
+
+html = requests.get(comediehumaine_url).content
+soup = BeautifulSoup(html, "html.parser")
+
+for book in tqdm(soup.find_all("li", {"class": "booklink"})):
+    text_id = book.a["href"]
+    title = book.find("span", {"class": "title"}).text
+    url = link_format.format(text_id=text_id)
+    resp = requests.get(url)
+    with open(title + ".txt", "w") as f:
+        f.write(resp.content.decode("utf-8"))
+
+
