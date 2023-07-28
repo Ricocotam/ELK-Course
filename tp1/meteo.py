690ed5bb68c45da8e0cadc9a14466dc821fd00d1
Ricocotam <ricocotam@gmail.com>
2023-07-28 09:28:15 +0200

    meteo.p

--- tp1/meteo.py
+++ tp1/meteo.py
@@ -1,14 +1,15 @@
 import os
+from uuid import uuid4
 import requests
 import pandas as pd
-from tqdm import trange
+from tqdm import trange, tqdm
 from glob import glob
 from elasticsearch.helpers import bulk
 from elasticsearch import Elasticsearch
 
 url = "https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/synop.{yyyy}{mm:02d}.csv.gz"
 os.mkdir("data")
-for yyyy in range(1996, 1999):
+for yyyy in range(1996, 2022):
     for mm in trange(1, 12):
         url_ = url.format(yyyy=yyyy, mm=mm)
         r = requests.get(url_)
@@ -17,10 +18,19 @@ for yyyy in range(1996, 1999):
 
 os.system("gzip -d data/*.gz")
 
-
 es = Elasticsearch("http://elastic:changeme@localhost:9200")
+def doc_generator(df, fn):
+    df_iter = df.iterrows()
+    for index, document in df_iter:
+        d = {
+                "_index": 'elk-meteo-0001',
+                "_op_type": "create",
+                "_id" : f"{uuid4()}",
+                "_source": document.to_json(),
+            }
+        yield d
+
+
 for fn in tqdm(glob("data/*.csv")):
-    df = pd.read_csv(fn)
-    df["_index"] = "elk-course-0001"
-    df["_op_type"] = "index"
-    bulk(es, df.to_json(orient="records")) 
+    df = pd.read_csv(fn, sep=";")
+    bulk(es, doc_generator(df, fn))
