e57b4a8a2309024d671e4a34746da190d7688818
Adrien Pouyet <Ricocotam@users.noreply.github.com>
2023-08-16 13:10:00 +0200

    Update produce_producer.py

--- tp2/produce_producer.py
+++ tp2/produce_producer.py
@@ -4,7 +4,7 @@ from glob import glob
 with open("data/output.log", "a") as logfile:
     ps = []
     print("output log openned")
-    for fn in glob("/myfiles/*.txt"):
+    for fn in glob("/myfiles/data/*.txt"):
         print(f"starting producer on {fn}")
         p = subprocess.Popen(["python", "/myfiles/producer.py", fn], stdout=logfile, stderr=logfile)
         ps.append(p)
